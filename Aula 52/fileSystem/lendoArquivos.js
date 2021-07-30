3fb41ae20ff80b0086906369c699998db1450ad4
alanis cibele <alaniscbele.sc@gmail.com>
2021-07-30 14:04:20 -0300

    node

--- Aula 52/fileSystem/lendoArquivos.js
+++ Aula 52/fileSystem/lendoArquivos.js
@@ -23,3 +23,23 @@ fs.readFile(path.resolve(__dirname, "teste.txt"), (err, data) => {
         console.log(err.message);
     }
 })();
+
+/*
+Exercício
+
+1) Crie um script que leia o arquivo exercioNomes.txt e mostra todos os nomes que começam com a letra A
+presentes no arquivo.
+
+2) Crie uma função getUserByName(name) que retorne o usuário obtido através do arquivo "users.json". Caso o usuário
+não exista a função deve retornar undefined.
+
+Se o usuário existir mostrar as informações do usuário no seguinte formato:
+Usuário encontrado: 
+nome: [nome do usuário]
+idade: [idade do usuário]
+email: [email do usuário]
+
+Caso o usuário não existir mostrar a mensagem: "Usuário não foi encontrado."
+
+Obs.: Nome e sobrenome.
+*/

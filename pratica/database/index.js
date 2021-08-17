295021df6196b9b39888afc8f3a0f61749dcd03c
alanis cibele <alaniscbele.sc@gmail.com>
2021-08-17 17:23:48 -0300

    sequelize

--- pratica/database/index.js
+++ pratica/database/index.js
@@ -1,12 +1,24 @@
-require("dotenv").config()
-const { Sequelize } = require("sequelize")
-const dbConfig = require("../config/database")
- 
-const sequelize = new Sequelize(dbConfig);
+require("dotenv").config();
+const { Sequelize } = require("sequelize");
+const config = require("../config/database");
+
+const Usuario = require("../models/Usuario");
+const Endereco = require("../models/Endereco");
+const Projeto = require("../models/Projeto");
+
+const sequelize = new Sequelize(config);
+
+// Inicializando os models
+Usuario.init(sequelize);
+Endereco.init(sequelize);
+Projeto.init(sequelize);
+
+// Definindo as associações
+Usuario.associate(sequelize.models);
+Endereco.associate(sequelize.models);
+Projeto.associate(sequelize.models);
 
 module.exports = sequelize;
-
-
 
 // (async () => {
 //     try {
--- pratica/models/Usuario.js
+++ pratica/models/Usuario.js
@@ -1,96 +1,58 @@
+const { DataTypes, Sequelize, Model } = require("sequelize");
+const bcrypt = require("bcrypt");
 
-const { DataTypes } = require("sequelize");
-const { hashSync, compareSync } = require("bcrypt");
-const sequelize = require("../database");
+class Usuario extends Model {
+    static init(sequelize) {
+        super.init({
+            id: {
+                type: DataTypes.UUID,
+                primaryKey: true,
+                defaultValue: Sequelize.UUIDV4,
+            },
+            nome: {
+                type: DataTypes.STRING,
+                allowNull: false
+            },
+            email: {
+                type: DataTypes.STRING,
+                allowNull: false,
+                unique: true,
+                validate: {
+                    isEmail: {
+                        msg: "E-mail precisa ser válido!"
+                    }
+                }
+            },
+            senha: {
+                type: DataTypes.STRING,
+                allowNull: false,
+                set(value) {
+                    this.setDataValue("senha", bcrypt.hashSync(value, 10));
+                }
+            }
+        }, {
+            sequelize,    
+        });
+    }
 
+    static associate(models) {
+        this.hasOne(models.Endereco, { foreignKey: "usuarioId" });
+        this.belongsToMany(models.Projeto, { 
+            through: "UsuariosProjetos", 
+            as: {
+                singular: "projeto",
+                plural: "projetos"
+            } 
+        });
+    }
 
-
-const Usuario = sequelize.define("Usuario", {
-nome: {
-type: DataTypes.STRING,
-allowNull: false
-},
-email: {
-type: DataTypes.STRING,
-allowNull: false,
-unique: true,
-validate: {
-isEmail: {
-msg: "E-mail inválido!"
+    checarSenha(senha) {
+        return bcrypt.compareSync(senha, this.senha);
+    }    
 }
-}
-},
-senha: {
-type: DataTypes.STRING,
-allowNull: false,
-set(value) {
-this.setDataValue("senha", hashSync(value, 10));
-}
-}
-});
-// }, {
-// tableName: "usuarios",
-// underscored: true
-// });
 
 
-
-(async () => {
-try {
-// Criando a tabela Usuarios
-await Usuario.sync({ force: true });
-console.log("Tabela criada com sucesso!");
+module.exports = Usuario;
 
 
-
-// Inserindo um usuário
-const pedro = await Usuario.create({
-nome: "Pedro",
-email: "pedro@email.com",
-senha: "123456"
-});
-console.log("Usuário criado com sucesso");
-console.log(pedro.toJSON());
-
-
-
-// Modificar o pedro
-pedro.email = "pedrao@email.com";
-await pedro.save();
-console.log("E-mail do pedro atualizado!");
-
-
-
-// Remover o pedro
-await pedro.destroy();
-console.log("Pedro foi removido do banco de dados");
-
-
-
-// Inserindo vários usuários
-const usuarios = await Usuario.bulkCreate([
-{
-nome: "Pedro",
-email: "pedro@email.com",
-senha: "123456"
-},
-{
-nome: "José",
-email: "jose@email.com",
-senha: "123456"
-}
-]);
-console.log("Usuários inseridos com sucesso!");
-usuarios.forEach(usuario => console.log(usuario.toJSON()));
-
-
-
-// Comparando a senha
-console.log(compareSync("12346", usuarios[0].toJSON().senha));
-} catch (err) {
-console.error("Ocorreu algum erro ao criar a tabela", err);
-} finally {
-sequelize.close();
-}
-})();
 
